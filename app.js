b54f5912b5cc76351bf20c360dd4468d326f4626
ekazubuike <ayo.azubuike@gmail.com>
2017-10-29 05:28:47 +0000

    Add mLab

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ const   commentsRoutes  = require('./routes/comments'),
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+// mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+mongoose.connect("mongodb://ek:ekyelpcamp@ds133290.mlab.com:33290/ek_yelpcamp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
